08beeb1d92f3ca759f0e87d5b14e10b46b583b4c
Hery-dev <89139631+Hery-dev@users.noreply.github.com>
2023-12-08 09:14:50 +0300

    Test commit

--- application/server/src/main/java/fr/customer/accounting/Application.java
+++ application/server/src/main/java/fr/customer/accounting/Application.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan
 public class Application {
     public static void main(String[] args) {
+        System.out.println("OK");
         SpringApplication.run(Application.class, args);
     }
 }
